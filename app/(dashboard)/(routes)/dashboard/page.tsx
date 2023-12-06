e2996722b15805286f535f2d7dacf367acf69d09
Liqing Pan <liqing.pan@edutrigger.com>
2023-12-06 18:48:56 -0500

    update the dahsboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
@@ -12,29 +13,39 @@ export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div>
+    <div className="grid-pattern">
+      <div className="pt-20"></div>
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+        <h2 className="text-2xl md:text-4xl font-bold text-center uppercase">
+          SELECT A TOOL TO generate
         </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+        {/* <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
-        </p>
+        </p> */}
       </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {tools.map((tool) => (
-          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
-            <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-8 h-8", tool.color)} />
+      <div className="px-4 md:px-20 lg:px-32">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {tools.map((tool) => (
+            <div
+              key={tool.href}
+              onClick={() => router.push(tool.href)}
+              className="border-black/5 hover:shadow-md transition cursor-pointer flex flex-col items-center justify-center p-4"
+            >
+              <div
+                className={`p-2 w-full rounded-md ${tool.bgColor} flex justify-center`}
+              >
+                <Image
+                  src={tool.icon}
+                  alt={tool.label}
+                  width={256}
+                  height={256}
+                  className={`${tool.color} rounded-lg`}
+                />
               </div>
-              <div className="font-semibold">
-                {tool.label}
-              </div>
+              <div className="font-semibold mt-2">{tool.label}</div>
             </div>
-            <ArrowRight className="w-5 h-5" />
-          </Card>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
